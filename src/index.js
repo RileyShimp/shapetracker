7c19114a54da3d297459c7adb13f8c9ff60971c8
Riley Shimp <rilryshimp@gmail.com>
2022-01-31 14:23:35 -0800

    modify idex.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Triangle from './triangle.js';
-import './css/styles.css';
 import $ from 'jquery';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
